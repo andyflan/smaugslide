e85d2e5d61de781e975a65ff5631b62704d3e15d
Andrew Flannery <andyflan@inheritweb.net>
2014-06-21 11:28:52 +0100

    Added scss/grunt/readme

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        
+      },
+      dist: {
+        options: {
+          //outputStyle: 'compressed'
+          outputStyle: 'expanded'
+        },
+        files: {
+          'css/screen.css': 'scss/screen.scss',
+          'css/smaugslide.css': 'scss/smaugslide.scss'
+        }
+      }
+    },
+    watch: {
+      grunt: {
+        files: ['Gruntfile.js']
+      },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      },
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          hostname: '0.0.0.0',
+          keepalive: true,
+          open: 'http://localhost:<%= connect.server.options.port %>'
+        }
+      }
+    },
+
+    concurrent: {
+      server: ['connect:server', 'watch'],
+
+      options: {
+        logConcurrentOutput: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['build','watch']);
+  grunt.registerTask('server', ['concurrent:server']);
+}
